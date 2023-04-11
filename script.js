f84e89364b8329baf3dc3b5cd611ee91e0ab7ab0
victormc13 <viktorsb14@gmail.com>
2023-04-11 16:22:57 -0500

    Create array for content cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Menu mobile
+// Menu mobile
 const menu = document.getElementById('menu');
 
 document.getElementsByClassName('menu-btn')[0].addEventListener('click', () => {
@@ -13,15 +13,44 @@ document.getElementsByClassName('menu-options')[0].addEventListener('click', () 
   menu.style.display = 'none';
 });
 
-//Projects
+// Projects
 
 const projects = [
   {
     id: 1,
     name: 'Keeping track of hundreds of components',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'image mobile': 'icons/Snapshoot Portfolio.svg',
+    'image desktop': 'icons/Snapshoot Portfolio desktop.svg',
     technologies: ['Ruby', 'CSS', 'JavaScript', 'Bootstrap'],
     liveVersion: '#',
     source: '#',
   },
-]
+  {
+    id: 2,
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'image mobile': 'icons/Snapshoot Portfolio.svg',
+    'image desktop': 'icons/Snapshoot Portfolio desktop.svg',
+    technologies: ['Ruby', 'CSS', 'JavaScript', 'Bootstrap'],
+    liveVersion: '#',
+    source: '#',
+  },
+  {
+    id: 3,
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'image mobile': 'icons/Snapshoot Portfolio.svg',
+    'image desktop': 'icons/Snapshoot Portfolio desktop.svg',
+    technologies: ['Ruby', 'CSS', 'JavaScript', 'Bootstrap'],
+    liveVersion: '#',
+    source: '#',
+  },
+  
+];
+
+const workSection = document.getElementById('work-section');
+
+projects.forEach(project, idx) => {
+  const  projectCards = document.createElement('div');
+  projectCards.classList = 'project-cards'};

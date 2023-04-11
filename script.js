d99eb5ef0eaff78a57b5fbf118959d8ecbd298c7
victormc13 <viktorsb14@gmail.com>
2023-04-11 12:36:26 -0500

    Add an array for the projects

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ document.getElementsByClassName('close-btn')[0].addEventListener('click', () => 
 document.getElementsByClassName('menu-options')[0].addEventListener('click', () => {
   menu.style.display = 'none';
 });
+
+//Projects
+
+const projects = [
+  {
+    id: 1,
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    technologies: ['Ruby', 'CSS', 'JavaScript', 'Bootstrap'],
+    liveVersion: '#',
+    source: '#',
+  },
+]

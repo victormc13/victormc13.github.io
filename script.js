a7bbc1f611486dea364779c72d733ddf7e85b1de
victormc13 <viktorsb14@gmail.com>
2023-04-10 23:35:02 -0500

    Add menu-mobile interaction whit javascript events

---
+++ script.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+
+document.getElementsByClassName('menu-btn')[0].addEventListener('click', () => {
+  menu.style.display = 'block';
+});
+
+document.getElementsByClassName('close-btn')[0].addEventListener('click', () => {
+  menu.style.display = 'none';
+});
+
+document.getElementsByClassName('menu-options')[0].addEventListener('click', () => {
+  menu.style.display = 'none';
+});

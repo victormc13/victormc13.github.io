8929b6700543616229ac1c3b30049baac4b2f628
victormc13 <viktorsb14@gmail.com>
2023-04-15 02:22:17 -0500

    Add the popup window and render the workSection with javascript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 // Menu mobile
 const menuBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
@@ -23,33 +24,33 @@ menuOptions.addEventListener("click", (event) => {
 const projects = [
   {
     id: 1,
-    name: "Keeping track of hundreds of components",
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "image mobile": "icons/Snapshoot Portfolio.svg",
-    "image desktop": "icons/Snapshoot Portfolio desktop.svg",
+    imagemobile: "icons/Snapshoot Portfolio.svg",
+    imagedesktop: "icons/Snapshoot Portfolio desktop.svg",
     technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
     liveVersion: "#",
     source: "#",
   },
   {
     id: 2,
-    name: "Keeping track of hundreds of components",
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "image mobile": "icons/Snapshoot Portfolio.svg",
-    "image desktop": "icons/Snapshoot Portfolio desktop.svg",
+    imagemobile: "icons/Snapshoot Portfolio.svg",
+    imagedesktop: "icons/Snapshoot Portfolio desktop.svg",
     technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
     liveVersion: "#",
     source: "#",
   },
   {
     id: 3,
-    name: "Keeping track of hundreds of components",
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "image mobile": "icons/Snapshoot Portfolio.svg",
-    "image desktop": "icons/Snapshoot Portfolio desktop.svg",
+    imagemobile: "icons/Snapshoot Portfolio.svg",
+    imagedesktop: "icons/Snapshoot Portfolio desktop.svg",
     technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
     liveVersion: "#",
     source: "#",
@@ -59,8 +60,8 @@ const projects = [
     name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "image mobile": "icons/Snapshoot Portfolio.svg",
-    "image desktop": "icons/Snapshoot Portfolio desktop.svg",
+    imagemobile: "icons/Snapshoot Portfolio.svg",
+    imagedesktop: "icons/Snapshoot Portfolio desktop.svg",
     technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
     liveVersion: "#",
     source: "#",
@@ -70,8 +71,8 @@ const projects = [
     name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "image mobile": "icons/Snapshoot Portfolio.svg",
-    "image desktop": "icons/Snapshoot Portfolio desktop.svg",
+    imagemobile: "icons/Snapshoot Portfolio.svg",
+    imagedesktop: "icons/Snapshoot Portfolio desktop.svg",
     technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
     liveVersion: "#",
     source: "#",
@@ -81,8 +82,8 @@ const projects = [
     name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "image mobile": "icons/Snapshoot Portfolio.svg",
-    "image desktop": "icons/Snapshoot Portfolio desktop.svg",
+    imagemobile: "icons/Snapshoot Portfolio.svg",
+    imagedesktop: "icons/Snapshoot Portfolio desktop.svg",
     technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
     liveVersion: "#",
     source: "#",
@@ -101,7 +102,7 @@ function renderWorkSection() {
   const titleWorkSection = document.createElement("h2");
   titleWorkSection.textContent = "My Recent Works";
   titleWorkSection.classList.add("title-work-section");
-  projectCardsContainer.insertAdjacentElement("beforebegin", titleWorkSection);
+  projectCardsContainer.insertAdjacentElement("beforebegin", titleWorkSection); //insert before to show up as title
 
   // Loop through each project and create a card for it
   projects.forEach((project) => {
@@ -136,58 +137,71 @@ function renderWorkSection() {
     if (projectCardsContainer.nodeType === Node.ELEMENT_NODE) {
       projectCardsContainer.insertAdjacentElement("beforeend", projectCard);
     }
-  });
 
-  // Render the shownProject
-  function renderShownProject(workSection) {
-    const shownprojectContainer = document.querySelector(
-      ".shown-project-container"
-    );
-    const showprojectContent = `<div class="shown-project flex-column">
-  <button class="close-shown-project"><img src="icons/Icon - Cancel project.svg" alt="close button" width="24px" height="24px"></button>
-  <img src="icons/Snapshoot Portfolio.svg" alt="project's image" />
-  <h3>Keeping track of hundreds of components</h3>
-  <ul class="technologies flex-row">
-    <li>Ruby on Rails</li>
-    <li>CSS</li>
-    <li>JavaScript</li>
-    <li>HTML</li>
-  </ul>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
-  </p>
-  <div class="project-buttons flex-row">
-    <button class="live-btn flex-row">See live <img src="icons/see live icon.svg" alt="see live icon"></button>
-    <button class="source-btn flex-row">See source <img src="icons/Icon -GitHub.svg" alt="github icon"></button>
-  </div>
-  </div>`;
-    if (shownprojectContainer.nodeType === Node.ELEMENT_NODE) {
-      shownprojectContainer.insertAdjacentHTML("beforeend", showprojectContent);
+    // Render the shownProject
+    function renderShownProject(workSection) {
+      const shownprojectContainer = document.querySelector(
+        ".shown-project-container"
+      );
+
+      for (const project of projects) {
+        const showprojectContent = `<div class="shown-project flex-column">
+      <button class="close-shown-project"></button>
+      <div class="project-image"></div>
+      <h3>${project.name}</h3>
+      <ul>${technologiesList.outerHTML}</ul>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+      </p>
+      <div class="project-buttons flex-row">
+        <button class="live-btn flex-row">See live <img src="icons/see live icon.svg" alt="see live icon"></button>
+        <button class="source-btn flex-row">See source <img src="icons/Icon -GitHub.svg" alt="github icon"></button>
+      </div>
+    </div>`;
+        if (shownprojectContainer.nodeType === Node.ELEMENT_NODE) {
+          shownprojectContainer.insertAdjacentHTML(
+            "beforeend",
+            showprojectContent
+          );
+        }
+      }
       workSection.insertAdjacentElement("beforeend", shownprojectContainer);
     }
-  }
+    renderShownProject(workSection);
 
-  renderShownProject(workSection);
+    // PopUp
+    const projectButtons = document.querySelectorAll(".project-btn");
+    const closeShownProject = document.querySelectorAll(".close-shown-project");
+    const popupContainer = document.querySelector(".shown-project-container");
 
-  const openPopupBtns = document.querySelectorAll(".project-btn");
-  const closePopupBtn = document.querySelector(".close-shown-project");
-  const popupContainer = document.querySelector(".shown-project-container");
-
-  openPopupBtns.forEach((openPopupBtn) => {
-    openPopupBtn.addEventListener("click", () => {
+    const openPopup = () => {
       popupContainer.style.display = "block";
-    });
-  });
+      document.body.style.overflow = "hidden";
+    };
 
-  closePopupBtn.addEventListener("click", () => {
-    popupContainer.style.display = "none";
-  });
-
-  popupContainer.addEventListener("click", (event) => {
-    if (event.target === popupContainer) {
+    const closePopup = () => {
       popupContainer.style.display = "none";
-    }
+      document.body.style.overflow = "";
+    };
+
+    const clickPopupContainer = (event) => {
+      if (event.target === popupContainer) {
+        closePopup();
+      }
+    };
+
+    //Opens the popup when click each button with the class project-btn
+    projectButtons.forEach((projectButton) => {
+      projectButton.addEventListener("click", openPopup);
+    });
+
+    closeShownProject.forEach((closeProjects) => {
+      closeProjects.addEventListener("click", closePopup);
+    });
+
+    popupContainer.addEventListener("click", clickPopupContainer);
   });
 }
 
-window.onload = renderWorkSection();
+window.onload = renderWorkSection;
+

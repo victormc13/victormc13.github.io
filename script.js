8d2b922dd5e3c3729f656cc7548c068b562c02e7
victormc13 <viktorsb14@gmail.com>
2023-04-16 23:52:37 -0500

    Finish validate contact-form

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-//Mobile menu
+// Mobile menu
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+const menuOptions = document.querySelector(".menu-options");
+const menu = document.getElementById("menu");
+
+// Open menu
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "block";
+});
+// Close menu
+closeBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+});
+// Close the menu if any link it's clicked
+menuOptions.addEventListener("click", (event) => {
+  if (event.target.tagName === "A") {
+    menu.style.display = "none";
+  }
+});
 
 // Projects
 const projects = [
